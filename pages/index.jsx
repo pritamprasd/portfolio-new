6ca57695f4e0ce43784ca3f1cd1280f9248efd5c
pritamprasd <mail@pritam.dev>
2022-09-16 01:16:46 +0530

    env files

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,10 @@ import {yamlToJson} from '../utils/transformers'
 import {configUrl} from '../utils/config'
 import Navbar from '../containers/navbar/Navbar.jsx';
 import MainCanvas from '../containers/main_canvas/MainCanvas';
+import styles from './index.module.css'
 
 export async function getStaticProps() {
+  console.log(`NEXT_PUBLIC_SAMPLE_ENV: ${process.env.NEXT_PUBLIC_SAMPLE_ENV}`)  
   const res = await fetch(configUrl).catch(err => console.error(`---1 ${err}`));
   const body = await res.text();  
   const config = yamlToJson(body)
@@ -16,10 +18,9 @@ export async function getStaticProps() {
 
 function Index({config}) {
   return(
-    <div>
-      <div>Welcome to Next.js! {config.title}</div>
-      <Navbar/>
-      <MainCanvas/>
+    <div className={styles.global_window}>
+      <Navbar className={styles.main_nav}/>
+      <MainCanvas className={styles.main_view}/>
     </div>
   )
 }
