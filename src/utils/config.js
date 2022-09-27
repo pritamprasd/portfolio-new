b5cfe089e6d013668d74c9b619db7a40e8244ac4
pritamprsad <mail@pritam.dev>
2022-09-27 20:04:44 +0530

    refreh config option for development

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,10 @@ const raw_id = "0b0c7efa229390164d6c6299b316bc54d2cdd249";
 export const configUrl = `https://gist.githubusercontent.com/pritamprasd/2383c2a6d34b6ae436412fbf82ccc457/raw/${raw_id}/site-config.yaml`
 
 export async function get_config() {
-    let config = getFromLS('config', 'json');    
+    let config = getFromLS('config', 'json');  
+    if(process.env.NEXT_PUBLIC_REFRESH_CONFIG_ALWAYS == "Yup"){
+        config = null;
+    }  
     if(config == null || process.env.NEXT_PUBLIC_ENABLE_CONFIG_CACHE === "Nope"){
         const res = await fetch(configUrl).catch(err => console.error(`---1 ${err}`));
         const body = await res.text();  
